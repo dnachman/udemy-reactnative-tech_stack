43dc1a81128edaad401b5054029b5b99bc62c68d
David Nachman <david@nachman.org>
2017-05-27 10:49:46 -0400

    basics of list

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    // render a single row
+    renderRow(library) {   // argument to function is the item it is expected to render
+        return <ListItem library={library} />;
+    }
+
     render() {
-        console.log(this.props);
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
